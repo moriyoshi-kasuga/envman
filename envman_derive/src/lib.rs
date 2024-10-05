ca40b0c17c022bfa2a7ce1584aabc8c18901a492
moriyoshi-kasuga <moriyoshi.kasuga1218@gmail.com>
2024-10-05 19:50:18 +0900

    refactor: Change to method reference for clarity

--- envman_derive/src/lib.rs
+++ envman_derive/src/lib.rs
@@ -3,6 +3,6 @@ use syn::{parse_macro_input, DeriveInput};
 #[proc_macro_derive(EnvMan, attributes(envman))]
 pub fn derive_envman(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     envman_derive_internals::derive_envman(parse_macro_input!(input as DeriveInput))
-        .unwrap_or_else(|err| err.to_compile_error())
+        .unwrap_or_else(syn::Error::into_compile_error)
         .into()
 }
